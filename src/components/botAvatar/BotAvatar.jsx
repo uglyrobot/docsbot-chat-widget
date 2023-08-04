818d308b4b1658732a97550516067a0aa09b109c
Ra-Jay <rjyansonx@gmail.com>
2023-08-04 22:08:42 +0800

    Allow using custom icon and avatar in widget

--- src/components/botAvatar/BotAvatar.jsx
+++ src/components/botAvatar/BotAvatar.jsx
@@ -1,20 +1,14 @@
-import React from "react";
-import { useConfig } from "../configContext/ConfigContext";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faComment,
-  faRobot,
-  faLifeRing,
-  faInfo,
-  faBook,
-} from "@fortawesome/free-solid-svg-icons";
-import { decideTextColor, getLighterColor } from "../../utils/colors";
+import React from "react"
+import { useConfig } from "../configContext/ConfigContext"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faComment, faRobot, faLifeRing, faInfo, faBook } from "@fortawesome/free-solid-svg-icons"
+import { decideTextColor, getLighterColor } from "../../utils/colors"
 
 export const BotAvatar = () => {
-  const { botIcon, color } = useConfig();
+  const { botIcon, color } = useConfig()
 
-  const bgColor = getLighterColor(color || "#1292EE", 0.6);
-  const fontColor = decideTextColor(bgColor);
+  const bgColor = getLighterColor(color || "#1292EE", 0.6)
+  const fontColor = decideTextColor(bgColor)
 
   //icon can be default, robot, life-ring, or question-circle
   const iconMap = {
@@ -23,30 +17,30 @@ export const BotAvatar = () => {
     "life-ring": faLifeRing,
     info: faInfo,
     book: faBook,
-  };
-
-  const iconToUse = iconMap[botIcon] || false;
-
-  if (iconToUse === false) {
-    return null;
   }
+
+  const iconToUse = iconMap[botIcon] || false
+  const isIconInList = iconMap.hasOwnProperty(botIcon)
+
   return (
     <div className="docsbot-chat-bot-avatar">
       <div
         className="docsbot-chat-bot-avatar-container"
         style={{
           backgroundColor: bgColor,
-        }}
-      >
+        }}>
         <p
           className="docsbot-chat-bot-avatar-icon"
           style={{
             color: fontColor,
-          }}
-        >
-          <FontAwesomeIcon icon={iconToUse} size="xs" />
+          }}>
+          {!botIcon || isIconInList ? (
+            <FontAwesomeIcon icon={iconToUse} size="xs" />
+          ) : (
+            <img src={botIcon} alt="Bot Avatar" />
+          )}
         </p>
       </div>
     </div>
-  );
-};
+  )
+}
